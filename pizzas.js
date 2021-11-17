3d42e626d6fe2edf475e469e62ccc4d412e14950
Breno Marques <90160591+soubreno@users.noreply.github.com>
2021-11-16 22:51:24 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Molho de tomate, camada dupla de mussarela e orégano.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Mussarela, requeijão, gorgonzola, orégano e parmesão ralado.'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Mussarela, pepperoni, champignon, cebola, orégano e pimentão verde.'},
+    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Mussarela, presunto, ovo de codorna, azeitona preta, cebola, orégano e pimentão verde.'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Mussarela, pepperoni, azeitona preta, parmesão ralado, orégano, Cream Cheese e alho granulado.'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Massa deliciosa recheada de muito chocolate e cobertura de creme de baunilha com granulado ou M&Ms.'}
+];
